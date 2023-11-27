be69efd339b31ee6c98071e4465ed14a8c64ceef
shchoholiev-opflo <s.shchoholiev@opflo.com>
2023-11-27 00:26:01 +0000

    Added header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layouts/header/header.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'smart-inventory-system-web';
+
+  public showHeaderAndFooter: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showHeaderAndFooter = !event.url.includes('/login') && !event.url.includes('/register');
+      }
+    });
+  }
 }
