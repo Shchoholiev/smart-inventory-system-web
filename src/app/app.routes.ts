49996b0f08e265174a276a3043e91fc94adfb6f3
shchoholiev <sh.codework@gmail.com>
2023-11-28 05:56:25 +0000

    Added shelf details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { ShelvesComponent } from './shelves/shelves/shelves.component';
 import { GroupCreationComponent } from './groups/group-creation/group-creation.component';
 import { GroupComponent } from './groups/group/group.component';
 import { DevicesComponent } from './devices/devices/devices.component';
+import { ShelfDetailComponent } from './shelves/shelf/shelf.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/shelves', pathMatch: 'full' },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
     { path: 'shelves', component: ShelvesComponent, canActivate: [AuthGuard] },
+    { path: 'shelves/:id', component: ShelfDetailComponent, canActivate: [AuthGuard] },
     { path: 'creategroup', component: GroupCreationComponent, canActivate: [AuthGuard] },
     { path: 'group', component: GroupComponent, canActivate: [AuthGuard] },
     { path: 'devices', component: DevicesComponent, canActivate: [AuthGuard] },
