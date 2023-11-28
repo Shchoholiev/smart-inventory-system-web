0fd19e1e8f945b265f30f8185e8d504358d6d498
shchoholiev <sh.codework@gmail.com>
2023-11-28 04:35:35 +0000

    Added shelves service

---
+++ src/app/shelves/shelf.model.ts
@@ -0,0 +1,7 @@
+export class Shelf {
+    id: string = '';
+    name: string = '';
+    isLitUp: boolean = false;
+    groupId: string = '';
+    deviceId: string = '';
+}
---
+++ src/app/shelves/shelves.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../network/api.service';
+import { Shelf } from './shelf.model';
+import { Item } from '../items/item.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShelvesService {
+  
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  getShelf(shelfId: string) {
+    return this.apiService.get<Shelf>(`/shelves/${shelfId}`);
+  }
+
+  getShelves(page: number, size: number, groupId: string) {
+    return this.apiService.get<any>(`/shelves?page=${page}&size=${size}&groupId=${groupId}`);
+  }
+
+  updateShelf(shelfId: string, shelf: Shelf) {
+    return this.apiService.put<Shelf>(`/shelves/${shelfId}`, shelf);
+  }
+
+  getShelfItems(shelfId: string) {
+    return this.apiService.get<any>(`/shelves/${shelfId}/items`);
+  }
+
+  controlLight(shelfId: string, itemId: string, isOn: boolean) {
+    return this.apiService
+      .patch<Shelf>(`/shelves/${shelfId}/status`, 
+        { 
+          isLitUp: isOn ,
+          itemId: itemId
+        });
+  }
+
+  addItem(shelfId: string, item: Item) {
+    return this.apiService.post<Item>(`/shelves/${shelfId}/items`, item);
+  }
+}
